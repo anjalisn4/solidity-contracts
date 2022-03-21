4926c7096a4990bc252dc853e70c801937b5d5b1
Anjali Sunder Naik <nuj2kor@bosch.com>
2022-03-21 16:52:07 +0530

    added sample react app

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,58 @@
+import _ from 'lodash';
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+
+import SearchBar from './components/search_bar';
+import youtubeApiSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyDkemru575S3SGEASdyKse7MR4sjHUof6I';
+
+
+
+// create a new component. This component should produce some HTML
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+        this.videoSearch('surfboards');
+
+    }
+
+    videoSearch(term) {
+        youtubeApiSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        })
+    }
+    // babe compiles JSX into vanila js which can be rendered into DOM
+    render() {
+
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term)
+        }, 300)
+        return (
+            <div>
+                <SearchBar onSearchTermChange={term => videoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
+};
+
+//  Take this component's generated HTML and put it on page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'))
